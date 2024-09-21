f7e37aec6ffa8280b14b08f628701f0bf01741e4
babybaghel7392 <103937760+babybaghel7392@users.noreply.github.com>
2024-09-21 14:34:24 +0530

    Delete Backend directory

--- Backend/User_Management_Portal/src/main/java/com/user/test/exception/UserException.java
+++
@@ -1,8 +0,0 @@
-package com.user.test.exception;
-
-public class UserException extends RuntimeException {
-		public UserException(Long id)
-		{
-			super("User not found with this id :"+ id);
-		}
-}
